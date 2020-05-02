bd7c6a8485fccee58a5407e74245a0376484ef30
Pedro Evangelista <pedroevangelista@outlook.com>
2020-05-02 13:29:45 -0300

    Create account screen

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -6,6 +6,7 @@ import {
   KeyboardAvoidingView,
   Platform,
 } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Feather';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -20,6 +21,8 @@ import {
 } from './styles';
 
 const SignIn: React.FC = () => {
+  const navigation = useNavigation();
+
   return (
     <>
       <KeyboardAvoidingView
@@ -55,11 +58,7 @@ const SignIn: React.FC = () => {
           </Container>
         </ScrollView>
       </KeyboardAvoidingView>
-      <CreateAccountButton
-        onPress={() => {
-          console.log('criar conta');
-        }}
-      >
+      <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
         <Icon name="log-in" size={20} color="#ff9000" />
         <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
       </CreateAccountButton>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,9 +8,7 @@ const AuthRoutes: React.FC = () => (
   <Auth.Navigator
     screenOptions={{
       headerShown: false,
-      cardStyle: {
-        backgroundColor: '#312e38',
-      },
+      cardStyle: { backgroundColor: '#312e38' },
     }}
   >
     <Auth.Screen name="SignIn" component={SignIn} />
